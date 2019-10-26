e0e6a9fb3e3d96edb471d1a3cd0cd88883a8c7a3
Alex Fischman <alex@fischman.org>
2019-10-25 17:32:47 -0700

    Reduced Main to MVP

--- Main.hs
+++ Main.hs
@@ -5,22 +5,13 @@ import Graphics.Gloss
 
 main :: IO ()
 main = display FullScreen white $ scale 40 40 $ pictures [
-            color black $ Line [(0, -10), (0, 10)],
-            color black $ Line [(-10, 0), (10, 0)],
-            color blue  $ graph f   (-10, 10, -10, 10) 0.1,
-            color green $ graph f'  (-10, 10, -10, 10) 0.1,
-            color red   $ graph f'' (-10, 10, -10, 10) 0.1,
-            color blue  $ graph g   (-10, 10, -10, 10) 0.1,
-            color green $ graph g'  (-10, 10, -10, 10) 0.1,
-            color red   $ graph g'' (-10, 10, -10, 10) 0.1
+            color black  $ Line [(0, -10), (0, 10)],
+            color black  $ Line [(-10, 0), (10, 0)],
+            color blue   $ graph f   (-10, 10, -10, 10) 0.1,
+            color green  $ graph f'  (-10, 10, -10, 10) 0.1
         ]
     where
-        f :: (Double -> Double)
-        f = sin
-        f' = derivative f
-        f'' = derivative f'
 
-        g :: (Rational -> Rational)
-        g x = 0.5 * x * x
-        g' = derivative g
-        g'' = derivative g'
+        f :: (Rational -> Rational)
+        f x = 0.5 * x * x
+        f'  = derivative f
